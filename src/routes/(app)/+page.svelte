e16aaf3f91a6f9b51eec25cf6a72ba023536e3b9
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2023-09-12 22:51:25 +0200

    changed the UI for the main page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -8,15 +8,18 @@
 
     function nextContact() { 
         // @ts-ignore
-        let currentContactIndex = contacts.findIndex(contact => contact.id == document.querySelector("input[type=hidden]").value);
-        let tmpContacts = contacts.slice(0, currentContactIndex).concat(contacts.slice(currentContactIndex + 1, contacts.length)); 
+        const currentContactIndex = contacts.findIndex(contact => contact.id == document.querySelector("input[type=hidden]").value);
+        const tmpContacts = contacts.slice(0, currentContactIndex).concat(contacts.slice(currentContactIndex + 1, contacts.length)); 
         const randomIndex = Math.floor(Math.random() * tmpContacts.length);
         // @ts-ignore
-        document.querySelector("input[type=tel]").placeholder = tmpContacts[randomIndex]["phone"];
+        document.querySelector(".phone").textContent = tmpContacts[randomIndex]["phone"];
         // @ts-ignore
         document.querySelector("input[type=hidden]").value = tmpContacts[randomIndex]["id"];
         // @ts-ignore
         document.getElementById("contact-name").innerHTML = `${tmpContacts[randomIndex]["name"]}`
+
+        // @ts-ignore
+        document.querySelector("input[type=tel]").value = "";
     }
 
 </script>
@@ -26,9 +29,10 @@
         <h1>You have no contacts</h1>
     {:else}
         <h1 id="contact-name">{contacts[0]["name"]}</h1>
+        <h2 class="phone">{contacts[0]["phone"]}</h2>
         <form on:submit|preventDefault={nextContact}>
             <input type="hidden" value={contacts[0].id}>
-            <input type="tel" placeholder={contacts[0]["phone"]}>
+            <input type="tel">
         </form>
     {/if}
 </Card>
@@ -40,6 +44,6 @@
         height: 50px;
         font-size: 2rem;
         outline: none;
-    }
+    } 
    
 </style>
