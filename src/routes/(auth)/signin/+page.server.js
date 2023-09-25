1908213cbbe7b9731fc6689e3ef8602fe993b46f
MaxTheTurtle0 <m.andrei.antonescu@gmail.com>
2023-09-25 06:06:25 +0200

    removed the layout.svelte since there was only 1 file using it at this point and moved the route protection from +layout.server.js to +page.server.js actually protecting the signin route properly now

--- src/routes/(auth)/signin/+page.server.js
+++ src/routes/(auth)/signin/+page.server.js
@@ -1,8 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 
-/** @type {import('./$types').LayoutServerLoad} */
-// @ts-ignore
+/** @type {import('./$types').PageServerLoad} */
 export const load = async (event) => {
     const session = await event.locals.getSession();
     if (session?.user) throw redirect(303, '/');
-};
+    // @ts-ignore
+    return {};
+}
--- src/routes/(auth)/signin/+page.svelte
+++ src/routes/(auth)/signin/+page.svelte
@@ -10,14 +10,63 @@
     <meta name="description" content="Sign in to NumberType">
 </svelte:head>
 
-<Card>
-    <h1>Sign In</h1>
-    <button type="button" class="github" on:click={() => signIn('github')}>GitHub</button>
-    <button type="button" class="google" on:click={() => signIn('google')}>Google</button>
-    <button type="button" class="twitter" on:click={() => signIn('twitter')}>Twitter</button>
-</Card>
+<main> 
+    <div class="side">
+        <div>
+            <h1>Welcome to NumberType!</h1>
+            <hr>
+            <p>You can store your contacts here and practice typing their numbers.</p>
+            <p>This way, you can learn to type numbers without looking at your keyboard!</p>
+        </div> 
+    </div>
+    <div class="content">
+        <Card>
+            <h1>Sign In</h1>
+            <button type="button" class="github" on:click={() => signIn('github')}>GitHub</button>
+            <button type="button" class="google" on:click={() => signIn('google')}>Google</button>
+            <button type="button" class="twitter" on:click={() => signIn('twitter')}>Twitter</button>
+        </Card>
+    </div>
+</main>
 
 <style>
+
+    main {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        height: 100dvh;
+    }
+
+    .side {
+        text-align: center;
+        color: #fff;
+        padding: 1rem;
+        background: -webkit-linear-gradient(-45deg, rgba(84, 0, 255, 1) 0, rgba(84, 0, 255, 1) 40%, rgba(140, 0, 255, 1) 40%, rgba(84, 0, 255, 1) 59%, rgba(84, 0, 255, 1) 59%, rgba(84, 0, 255, 1) 100%), -webkit-linear-gradient(-45deg, rgba(84, 0, 255, 1) 0, rgba(84, 0, 255, 1) 7%, transparent 7%, transparent 100%), -webkit-linear-gradient(-225deg, rgba(84, 0, 255, 1) 0, rgba(84, 0, 255, 1) 7%, transparent 7%, transparent 100%), rgba(84, 0, 255, 1); 
+    }
+
+    .side div {
+        font-size: 1.25rem;
+        align-items: flex-start; 
+    }
+
+    hr {
+        width: 100%;
+        border: 1px solid #eee;
+    }
+
+    @media(max-width: 680px) {
+        main {
+            grid-template-columns: 1fr;
+        }
+    }
 
     .github {
         background-color: #333;
